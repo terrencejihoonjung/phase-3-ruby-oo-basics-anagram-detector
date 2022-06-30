be322e7a970048843dd0b8f31f69ee0e4f278047
--replace-all <terrencejung928@gmail.com>
2022-06-30 11:42:43 -0700

    commit tree 5327895f39735a8eb57982ef6515a1840f68bf28

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        matches = []
+        array.each do |word|
+            if word.chars.sort == @word.chars.sort
+                matches << word
+            end
+        end
+        matches
+    end
+   
+end
